a6fd708e7c5224e73b64f3ad79f91f30822acd96
SpikeBonjour <triphuong@cbnteck.org>
2023-11-09 23:44:05 +0700

    tmpcommit

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # Copyright (C) 2023  Nguyễn Tri Phương
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -23,6 +23,8 @@ def main():
     if "credentials.json" not in os.listdir():
         with open("credentials.json", "w") as f:
             f.write(os.environ.get("credentials_gcloud_sa"))
+            print(f"written {os.environ.get('credentials_gcloud_sa')}")
+    print(os.listdir())
     cfg.init_config_hive()
     bot = phoenix.Bot()
     for f in os.listdir("modules"):
